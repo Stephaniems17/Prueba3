29355387a9f694b93ec0253381d6b3902413e003
Stephaniems17 <smms2217@gmail.com>
2025-03-14 09:50:59 -0500

    Add files via upload

---
+++ data/Fotos_3.js
@@ -0,0 +1,8 @@
+var json_Fotos_3 = {
+"type": "FeatureCollection",
+"name": "Fotos_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "PARQUE LOS FICUS", "Foto 1": "C:/Users/smms_/Pictures/fotos/1da5cbe1-4db2-4aec-b901-d87c7fad2e40.jfif", "Foto 2": "C:/Users/smms_/Pictures/fotos/983fd355-a89a-462d-8b31-2b705e94937b.jfif", "Foto 3": "C:/Users/smms_/Pictures/fotos/1671a1b5-66c9-41b2-9830-63cb5959c33c.jfif", "Foto 4": "C:/Users/smms_/Pictures/fotos/7498fbd5-8585-4d2b-be8b-d8b303144182.jfif" }, "geometry": { "type": "Point", "coordinates": [ -77.001810203498152, -12.002122866903283 ] } }
+]
+}
